d4237d47151079e76413a7312da8f228208fb034
jeffrey <jeffrey@kanawaconsulting.com>
2019-03-05 15:01:29 -0700

    Fixed vertical scaling of chat pane and moved style to external file

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+
+app.use(express.static('static'));
 
 app.get('/', function(req, res)
 {
@@ -9,12 +13,12 @@ app.get('/', function(req, res)
 
 io.on('connection', function(socket)
 {
+    console.log('a user connected');
     socket.on('chat message', function(msg)
     {
         console.log('message: ' + msg);
         io.emit('chat message', msg);
     });
-    console.log('a user connected');
     socket.on('disconnect', function()
     {
         console.log('a user disconnected');
